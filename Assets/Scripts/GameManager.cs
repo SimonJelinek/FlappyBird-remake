c60696332bc02487d509dbb698ae562b33e56695
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-04-18 21:20:47 +0200

    Added player instantiate

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject player;
+    public Transform parent;
+
     void Start()
     {
         App.gameManager = this;
         App.screenManager.Show<MenuScreen>();
         App.screenManager.Hide<InGameScreen>();
         App.screenManager.Hide<FailScreen>();
+    }
+
+    public void InstantiateMap()
+    {
+        Instantiate(player, new Vector3(), Quaternion.identity, parent);
     }
 }
