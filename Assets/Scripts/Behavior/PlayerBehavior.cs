1b5427208fd23c01642cae7717461a822699408b
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-04-18 21:25:53 +0200

    Added PlayerBehavior

---
+++ Assets/Scripts/Behavior/PlayerBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector2(0,5);
+        }
+    }
+}
