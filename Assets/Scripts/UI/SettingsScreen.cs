f134fb54dd1067f3340664359a7483f8bdcea5ed
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-04-19 18:54:46 +0200

    Added SettingsScreen

---
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SettingsScreen : ScreenBase
+{
+    public TMP_Text highscoreTxt;
+    bool opened = false;
+
+    public void OnSettingClick()
+    {
+        opened = !opened;
+
+        if (!opened)
+        {
+            App.screenManager.Show<SettingsScreen>();
+        }
+        else
+        {
+            App.screenManager.Hide<SettingsScreen>();
+        }
+
+        UpdateTxt();
+    }
+
+    void UpdateTxt()
+    {
+        highscoreTxt.text = PlayerPrefs.GetInt("highscore").ToString();
+    }
+}
